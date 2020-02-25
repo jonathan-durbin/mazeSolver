d84bc524e210d500bb5833882a577c06cf23a3c7
jonathan-durbin <jongdurbin@gmail.com>
2020-02-24 21:14:26 -0500

    Add checks against matrix edges
    Also removed comments and finalized the file.

--- solver.jl
+++ solver.jl
@@ -1,24 +1,18 @@
 using FileIO
 using Images
-# from timeit import default_timer as timer
-# import pygame
-# import pygame.gfxdraw
+using Logging
+
+if length(ARGS) > 0
+    if "-debug" in ARGS
+        global_logger(SimpleLogger(stdout, Logging.Debug))
+    end
+end
 
 # Translated from: https://github.com/Azideon/mazeSolver
 
 
-# """
-# Create a maze object.
-
-# Likely not needed. We'll see.
-# """
-# function mazeCreator(m)
-#     maze = map(i -> i == RGB{N0f8}(0,0,0) ? "B" : "W", m)
-# end
-
-
 """
-Find the entry and exit points of the maze.
+Find the entry and exit points of a maze.
 
 TODO: write this function better.
 
@@ -29,19 +23,19 @@ Assumptions:
 
 Returns entryY, entryX, exitY, exitX
 """
-function findPoints(m, white)
+function findPoints(m::Array{RGB{N0f8},2}, white::RGB{N0f8})
     start =  [1, 1]
-    finish = [1, 1]
+    finish = [size(m)...]
     for (i, val) in enumerate(m[1, :])  # top row
         val == white && (start[2] = i; break)
     end
     for (i, val) in enumerate(m[:, 1])  # left side
         val == white && (start[1] = i; break)
     end
-    for (i, val) in enumerate(m[end, :])  # bottom row
+    for (i, val) in Iterators.reverse(enumerate(m[end, :]))  # bottom row
         val == white && (finish[2] = i; break)
     end
-    for (i, val) in enumerate(m[:, end])  # right side
+    for (i, val) in Iterators.reverse(enumerate(m[:, end]))  # right side
         val == white && (finish[1] = i; break)
     end
     return start, finish
@@ -54,17 +48,19 @@ Returns:
     - solution (if solved)
     - false (if not)
 """
-function solve(maze, start, finish, white)
+function solve(maze::Array{RGB{N0f8},2}, start::Array{Int}, finish::Array{Int}, white::RGB{N0f8})
+    @info "Started solving..."
     solution = []
     washere = []
-    y, x = start
+    start[1] == 1 ? (y = 1+start[1]) : (y = start[1])
+    start[2] == 1 ? (x = 1+start[2]) : (x = start[2])
     while [y, x] != finish
         push!(washere, [y, x])
         push!(solution, [y, x])
-        if maze[y - 1, x] == white && !([y - 1, x] in washere)
+        if maze[y - 1, x] == white && [y - 1, x] != start && !([y - 1, x] in washere)
             @debug "Up"
             possibleSteps = [y - 1, x]
-        elseif maze[y, x + 1] == white && !([y, x + 1] in washere)
+        elseif maze[y, x + 1] == white && [y - 1, x] != start && !([y, x + 1] in washere)
             @debug "Right"
             possibleSteps = [y, x + 1]
         elseif maze[y + 1, x] == white && !([y + 1, x] in washere)
@@ -78,70 +74,29 @@ function solve(maze, start, finish, white)
             pop!(solution)
             possibleSteps = [solution[end][1], solution[end][2]]
             pop!(solution)
-            # visualiseSolver(x, y, (255, 255, 255))
         end
         y = possibleSteps[1]
         x = possibleSteps[2]
     end
     push!(solution, finish)
-    # visualiseSolver(finish[1], finish[2], (255, 0, 0))
-    # print("Solution:", solution)
+
     return solution
 end
-
-# def printImage():
-#     for x in solution:
-#         pix[x[1], x[0]] = (255, 0, 0)
-#     mazeImage.save("mazeSol.png")
-# def visualise():
-#     pygame.init()
-#     windowSize = 1920, 1080
-#     screen = pygame.display.set_mode(windowSize, pygame.FULLSCREEN | pygame.DOUBLEBUF)
-#     screen.set_alpha(None)
-#     pygame.display.set_caption('Maze Solver')
-#     screen.fill((255, 255, 255))
-#     return screen
-# def visualiseMaze():
-#     for y in range(0, len(maze)):
-#         for x in range(0, len(maze[y])):
-#             if maze[y][x] == "B":
-#                 rect = pygame.Rect((x * scaleFactor) + offset, (y * scaleFactor) + offset, scaleFactor, scaleFactor)
-#                 pygame.draw.rect(screen, (0, 0, 0), rect)
-#                 pygame.event.pump()
-#     pygame.display.flip()
-# def visualiseSolver(x, y, color):
-#     rect = pygame.Rect((x * scaleFactor) + offset, (y * scaleFactor) + offset, scaleFactor, scaleFactor)
-#     pygame.draw.rect(screen, color, rect)
-#     pygame.display.update(rect)
-#     pygame.event.pump()
 
 
 function main()
     maze = load("mazeSmall.png")
-    black = RGB{N0f8}(0, 0, 0)
-    white = RGB{N0f8}(0.0, 0.0, 0.0)
-    # scaleFactor = 4
-    # offset = 2
+    black = RGB(0, 0, 0)
+    white = RGB(1, 1, 1)
     start, finish = findPoints(maze, white)
-    println("Entry:, $start")
-    println("Exit:, $finish")
-    # screen = visualise()
-    # visualiseMaze()
-    # visualiseSolver(entryX, entryY, (0, 255, 0))
-    # visualiseSolver(exitX, exitY, (0, 255, 0))
+    println("Image size:, $(size(maze))")
+    println("Entry: $start")
+    println("Exit: $finish")
     starttime = time()
     solution = solve(maze, start, finish, white)
-    # printImage()
 
     println("Time taken: $(round(time() - starttime, digits=4))")
 
-    # running = True
-
-    # while running:
-    #     pygame.display.flip()
-    #     for event in pygame.event.get():
-    #         if event.type == pygame.QUIT:
-    #             running = False
     return solution
 end
 
